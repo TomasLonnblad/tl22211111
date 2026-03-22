3e6a050339c33eda5d6e77298c00de57792a052e
TomasLonnblad <tomaslonnblad@gmail.com>
2026-03-22 08:28:47 +0100

    x

---
+++ app/page.tsx
@@ -0,0 +1,17 @@
+import { BubbleBackground } from '@/components/animate-ui/components/backgrounds/bubble';
+
+type BubbleBackgroundDemoProps = {
+  interactive: boolean;
+};
+
+export const BubbleBackgroundDemo = ({
+  interactive,
+}: BubbleBackgroundDemoProps) => {
+  return (
+    <BubbleBackground
+      interactive={interactive}
+      className="absolute inset-0 flex items-center justify-center rounded-xl"
+    />
+  );
+};
+export default BubbleBackgroundDemo;
